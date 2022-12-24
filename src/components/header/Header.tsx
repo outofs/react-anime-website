9efcb04bcb0bcfb4a84c801b0319bd002d303637
outofs <rtranscendental@gmail.com>
2022-12-24 23:25:55 +0200

    creating header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,9 +1,54 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
+import "./header.scss";
+import { Link, useLocation } from "react-router-dom";
+
+const headerNav = [
+  { display: "Home", path: "/" },
+  { display: "Movie", path: "/movie" },
+  { display: "TV Series", path: "/series" },
+];
 
 const Header = () => {
+  const { pathname } = useLocation();
+  const headerRef = useRef<HTMLElement>(null);
+
+  const active = headerNav.findIndex((e) => e.path === pathname);
+
+  useEffect(() => {
+    const shrinkHeader = () => {
+      if (
+        document.body.scrollTop > 100 ||
+        document.documentElement.scrollTop > 100
+      ) {
+        headerRef.current?.classList.add("shrink");
+      } else {
+        headerRef.current?.classList.add("shrink");
+      }
+    };
+    window.addEventListener("scroll", shrinkHeader);
+    return () => {
+      window.removeEventListener("scroll", shrinkHeader);
+    };
+  }, []);
+
   return (
-    <header className="header" data-header>
-      Header
+    <header ref={headerRef} className="header">
+      <div className="header__wrap container">
+        <div className="logo">
+          <img
+            src={require("../../assets/1200px-Sharingan_triple.png")}
+            alt=""
+          />
+          <Link to="/">Anime</Link>
+        </div>
+        <ul className="header__nav">
+          {headerNav.map((e, i) => (
+            <li key={i} className={`${i === active ? "active" : ""}`}>
+              <Link to={e.path}>{e.display}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </header>
   );
 };
